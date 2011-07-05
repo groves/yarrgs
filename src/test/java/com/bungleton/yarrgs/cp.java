eb25ca2080ee704c6bd8380f0bb4e3abf8fb3b19
Charlie Groves <charlie.groves@gmail.com>
2011-07-05 15:00:22 -0700

    Add some usage and a main to let me use this as an example

--- src/test/java/com/bungleton/yarrgs/cp.java
+++ src/test/java/com/bungleton/yarrgs/cp.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class cp
 {
+    @Usage("Should subdirectories of directores in sourceFiles be copied")
     public boolean recursive;
 
-    @Positional(0) @Unmatched
+    @Usage("Files and directories to be copied") @Positional(0) @Unmatched
     public List<File> sourceFiles;
 
-    @Positional(-1)
+    @Usage("Directory to which sourceFiles should be copied") @Positional(-1)
     public File destination;
+
+    public static void main (String[] args)
+    {
+        cp cp = Yarrgs.parseInMain(cp.class, args);
+        System.out.println("Recursive: " + cp.recursive);
+        System.out.println("Source files: " + cp.sourceFiles);
+        System.out.println("Dest: " + cp.destination);
+    }
 }
